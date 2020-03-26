a54c8897f063027a6d08d7f144815a81e0aa4bde
Daniel Senff <mail@danielsenff.de>
2020-03-26 08:15:47 +0100

    fix date format to get identified by the spreadsheet

--- lib/wanikani/stat/aggregate.rb
+++ lib/wanikani/stat/aggregate.rb
@@ -10,7 +10,7 @@ module Wanikani
                     :enlighten_radicals, :enlighten_kanji, :enlighten_vocabulary,
                     :burned_radicals, :burned_kanji, :burned_vocabulary
 
-      COLUMN_MAPPING = %w[created_at level apprentice_radicals apprentice_kanji apprentice_vocabulary
+      COLUMN_MAPPING = %w[formatted_created_at level apprentice_radicals apprentice_kanji apprentice_vocabulary
                     guru_radicals guru_kanji guru_vocabulary
                     master_radicals master_kanji master_vocabulary
                     enlighten_radicals enlighten_kanji enlighten_vocabulary
@@ -18,6 +18,12 @@ module Wanikani
 
       def initialize
         @created_at = Time.now
+      end
+
+      def formatted_created_at
+        return nil unless created_at
+
+        created_at.strftime("%d.%m.%Y %k:%M")
       end
 
       def column_mapping
